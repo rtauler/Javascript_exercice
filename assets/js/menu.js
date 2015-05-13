3ba41565f4700bb3928e3570c3d4fcc05172c17b
Macmini <macmini@Macminis-Mac-mini.local>
2015-05-13 16:53:45 +0200

    añadido de ejercicio semi-acabado

---
+++ assets/js/menu.js
@@ -0,0 +1,28 @@
+var $a = $("ul.sub-menu");
+
+$(document).ready(function(){
+
+	// START ANTI-BUG MOBILE -> DESKTOP
+    
+    // END ANTI-BUG MOBILE -> DESKTOP
+
+	// START FADE IN/OUT HOVER
+    $("li", this).hover(function(){
+        $('ul.sub-menu', this).fadeToggle();
+    });
+    // END FADE IN/OUT HOVER
+
+    // START CURRENT MENU KEEPER
+    $("a", this).click(function(){
+    	$("a").parent().removeClass("current-menu-item");
+        $(this).parent().addClass("current-menu-item");
+    });
+    // END CURRENT MENU KEEPER
+
+    //START MENU TO CLOSE/CLOSE TO MENU
+    $("#mobile-button, #mobile-close").click(function(){
+    	$("#mobile-button, #mobile-close").toggle();
+    	$("#menu-container").toggle();
+    });
+    //END MENU TO CLOSE/CLOSE TO MENU
+});
