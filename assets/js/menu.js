00ec774d26a9bf0cd73adca4a9832750c05f836f
Macmini <macmini@Macminis-Mac-mini.local>
2015-05-18 09:20:33 +0200

    arreglo bug mobile->Desktop

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,27 +2,35 @@ var $a = $("ul.sub-menu");
 
 $(document).ready(function(){
 
-	// START ANTI-BUG MOBILE -> DESKTOP
+    // START ANTI-BUG MOBILE -> DESKTOP
     
     // END ANTI-BUG MOBILE -> DESKTOP
 
-	// START FADE IN/OUT HOVER
+    // START FADE IN/OUT HOVER
     $("li", this).hover(function(){
-        $('ul.sub-menu', this).fadeToggle();
+        $('ul.sub-menu', this).fadeIn();
+    },function(){
+        var submenu = $('ul.sub-menu', this);
+        
+        submenu.fadeOut({
+            complete: function () {
+                submenu.css('display', 'none');
+            }
+        });
     });
     // END FADE IN/OUT HOVER
 
     // START CURRENT MENU KEEPER
     $("a", this).click(function(){
-    	$("a").parent().removeClass("current-menu-item");
+        $("a").parent().removeClass("current-menu-item");
         $(this).parent().addClass("current-menu-item");
     });
     // END CURRENT MENU KEEPER
 
     //START MENU TO CLOSE/CLOSE TO MENU
     $("#mobile-button, #mobile-close").click(function(){
-    	$("#mobile-button, #mobile-close").toggle();
-    	$("#menu-container").toggle();
+        $("#mobile-button, #mobile-close").toggle();
+        $("#menu-container").toggle();
     });
     //END MENU TO CLOSE/CLOSE TO MENU
 });
